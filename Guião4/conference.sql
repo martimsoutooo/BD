f1c57ef138cd85e4cd39ebddc27262c78f2cde80
martimsoutooo <santos.martim@ua.pt>
2024-03-15 18:39:02 +0000

    fix nos sql

--- Guião4/conference.sql
+++ Guião4/conference.sql
@@ -1,56 +1,58 @@
-CREATE TABLE Instituicao (
+CREATE SCHEMA conference
+GO
+CREATE TABLE conference.Instituicao (
     Nome VARCHAR(50) NOT NULL,
     Endereco VARCHAR(50),
     PRIMARY KEY (Nome)
 );
 
-CREATE TABLE Autor (
+CREATE TABLE conference.Autor (
     Email VARCHAR(50) NOT NULL,
     Nome VARCHAR(50) NOT NULL,
     Instituicao_Nome VARCHAR(50),
-    FOREIGN KEY (Instituicao_Nome) REFERENCES Instituicao(Nome),
+    FOREIGN KEY (Instituicao_Nome) REFERENCES conference.Instituicao(Nome),
     PRIMARY KEY (Email)
 );
 
-CREATE TABLE ArtigoCientifico (
+CREATE TABLE conference.ArtigoCientifico (
     Numero_de_Registro INT NOT NULL,
     Titulo VARCHAR(50) NOT NULL,
     Autor_Email VARCHAR(50) NOT NULL,
-    FOREIGN KEY (Autor_Email) REFERENCES Autor(Email),
+    FOREIGN KEY (Autor_Email) REFERENCES conference.Autor(Email),
     PRIMARY KEY (Numero_de_Registro)
 );
 
-CREATE TABLE Conferencia (
+CREATE TABLE conference.Conferencia (
     Artigo_FK INT,
-    FOREIGN KEY (Artigo_FK) REFERENCES ArtigoCientifico(Numero_de_Registro)
+    FOREIGN KEY (Artigo_FK) REFERENCES conference.ArtigoCientifico(Numero_de_Registro)
 );
 
-CREATE TABLE Participantes (
+CREATE TABLE conference.Participantes (
     Email VARCHAR(50) NOT NULL,
     Instituicao_Nome VARCHAR(50),
     Morada VARCHAR(50),
     Nome VARCHAR(50) NOT NULL,
     Data_de_Inscricao DATE NOT NULL,
-    FOREIGN KEY (Instituicao_Nome) REFERENCES Instituicao(Nome),
+    FOREIGN KEY (Instituicao_Nome) REFERENCES conference.Instituicao(Nome),
     PRIMARY KEY (Email)
 );
 
-CREATE TABLE NaoEstudante (
+CREATE TABLE conference.NaoEstudante (
     Email VARCHAR(50) NOT NULL,
     Morada VARCHAR(50),
     Nome VARCHAR(50) NOT NULL,
     Data_de_Inscricao DATE NOT NULL,
     Referencia_Transacao VARCHAR(50),
-    FOREIGN KEY (Email) REFERENCES Participantes(Email),
+    FOREIGN KEY (Email) REFERENCES conference.Participantes(Email),
     PRIMARY KEY (Email)
 );
 
-CREATE TABLE Estudante (
+CREATE TABLE conference.Estudante (
     Email VARCHAR(50) NOT NULL,
     Instituicao_Nome VARCHAR(50)  NOT NULL,
     Morada VARCHAR(50)  NOT NULL,
     Nome VARCHAR(50) NOT NULL,
     Data_de_Inscricao DATE NOT NULL,
-    FOREIGN KEY (Email) REFERENCES Participantes(Email),
+    FOREIGN KEY (Email) REFERENCES conference.Participantes(Email),
     PRIMARY KEY (Email)
 );
--- Guião4/medicine.sql
+++ Guião4/medicine.sql
@@ -2,51 +2,54 @@ CREATE SCHEMA medicine;
 
 GO
 
-CREATE TABLE Paciente (
-    N_Utente INT PRIMARY KEY NOT NULL,
-    Data_Nascimento DATE NOT NULL,
-    Endereco VARCHAR(50) NOT NULL,
-    Nome VARCHAR(20) NOT NULL,
-    ID_Medico INT NOT NULL,
-    FOREIGN KEY (ID_Medico) REFERENCES Medico(ID)
-);
-
-CREATE TABLE Farmacia (
+CREATE TABLE medicine.Farmacia (
     NIF INT PRIMARY KEY NOT NULL,
     Telefone VARCHAR(20) NOT NULL,
     Endereco VARCHAR(50) NOT NULL,
     Nome VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE Medico (
+CREATE TABLE medicine.Medico (
     ID INT PRIMARY KEY NOT NULL,
     Especialidade VARCHAR(20) NOT NULL,
     Nome VARCHAR(20)NOT NULL
 );
 
-CREATE TABLE Prescricao (
+CREATE TABLE medicine.Farmaceutica (
+    Numero_Registo INT PRIMARY KEY NOT NULL,
+    Telefone VARCHAR(20) NOT NULL,
+    Endereco VARCHAR(50) NOT NULL,
+    Nome VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE medicine.Paciente (
+    N_Utente INT PRIMARY KEY NOT NULL,
+    Data_Nascimento DATE NOT NULL,
+    Endereco VARCHAR(50) NOT NULL,
+    Nome VARCHAR(20) NOT NULL,
+    ID_Medico INT NOT NULL,
+    FOREIGN KEY (ID_Medico) REFERENCES medicine.Medico(ID)
+);
+
+CREATE TABLE medicine.Prescricao (
     Numero INT PRIMARY KEY NOT NULL,
     Data DATE NOT NULL,
     ID_Medico INT NOT NULL,
     N_Paciente INT NOT NULL,
     Nome_Farmaco VARCHAR(20) NOT NULL,
     NIF_Farmacia INT NOT NULL,
-    FOREIGN KEY (ID_Medico) REFERENCES Medico(ID),
-    FOREIGN KEY (N_Paciente) REFERENCES Paciente(N_Utente),
-    FOREIGN KEY (NIF_Farmacia) REFERENCES Farmacia(NIF)
+    FOREIGN KEY (ID_Medico) REFERENCES medicine.Medico(ID),
+    FOREIGN KEY (N_Paciente) REFERENCES medicine.Paciente(N_Utente),
+    FOREIGN KEY (NIF_Farmacia) REFERENCES medicine.Farmacia(NIF)
 );
 
-CREATE TABLE Farmaco (
+CREATE TABLE medicine.Farmaco (
     Nome_Comercial VARCHAR(20) PRIMARY KEY NOT NULL,
     Formula VARCHAR(100) NOT NULL,
     NIF_Farmacia INT NOT NULL,
     N_Registo_Farm INT NOT NULL,
-    FOREIGN KEY (NIF_Farmacia) REFERENCES Farmacia(NIF)
+    FOREIGN KEY (NIF_Farmacia) REFERENCES medicine.Farmacia(NIF),
+	FOREIGN KEY (N_Registo_Farm) REFERENCES medicine.Farmaceutica(Numero_Registo)
 );
 
-CREATE TABLE Farmaceutica (
-    Numero_Registo INT PRIMARY KEY NOT NULL,
-    Telefone VARCHAR(20) NOT NULL,
-    Endereco VARCHAR(50) NOT NULL,
-    Nome VARCHAR(20) NOT NULL
-);
+
